08a802f410a2730b75e7b47b5e190536b59d15f8
ADMINRG-G7IFNSA\admin <ramesh.k@zoylo.com>
2020-05-11 14:33:23 +0530

    Fifth Commit

--- helloworld/src/main/java/com/techinfo/controller/HelloController.java
+++ helloworld/src/main/java/com/techinfo/controller/HelloController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Ramesh.K
+ *
+ */
 @RestController
 @RequestMapping("/api")
-
 public class HelloController {
 
+	/**
+	 * @author Ramesh.K
+	 * @description Api to get hello world message
+	 * @return String
+	 */
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World !!!";
