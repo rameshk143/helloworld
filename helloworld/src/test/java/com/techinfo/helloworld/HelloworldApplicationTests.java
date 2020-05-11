fb451edde576a3aaaf6e8fa4783da247786f5c49
ADMINRG-G7IFNSA\admin <ramesh.k@zoylo.com>
2020-05-11 13:02:49 +0530

    first commit

---
+++ helloworld/src/test/java/com/techinfo/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techinfo.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
